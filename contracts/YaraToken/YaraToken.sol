39d6f259eb6ed2b1682c902086f601f2099707cc
yaroslav2308 <yaroslav.monastyrev@gmail.com>
2022-10-24 20:34:27 +0300

    Initial supply parameter in constructor

--- contracts/YaraToken/YaraToken.sol
+++ contracts/YaraToken/YaraToken.sol
@@ -2,7 +2,13 @@
 pragma solidity ^0.8.4;
 
 import "../ERC20/ERC20.sol";
+import "../Other/Ownable.sol";
+import "../ERC20/ERC20Burnable.sol";
 
-contract YaraToken is ERC20 {
-    constructor() ERC20("YaraToken", "YTK") {}
+contract YaraToken is ERC20, Ownable, ERC20Burnable {
+    constructor(uint initialSupply_) ERC20("YaraToken", "YTK", initialSupply_) {}
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
 }
